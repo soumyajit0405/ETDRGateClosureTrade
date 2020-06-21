f9e3f8e0d959de5605be264b97454aaca72a587c
soumyajit0405 <soumyajit0405@gmail.com>
2020-06-21 19:44:54 +0530

    Changes

--- src/ScheduleDAO.java
+++ src/ScheduleDAO.java
@@ -18,6 +18,8 @@ public class ScheduleDAO {
 	PreparedStatement pStmt = null;
 	 public ArrayList<HashMap<String,Object>> getEvents(String date, String time) throws SQLException, ClassNotFoundException
 	 {
+		 ArrayList<HashMap<String,Object>> al=new ArrayList<>();
+	try {
 		 PreparedStatement pstmt = null;
 		  time=time+":00";
 		 
@@ -26,15 +28,18 @@ public class ScheduleDAO {
 				con = JDBCConnection.getOracleConnection();
 		 }
 		//	System.out.println("select aso.sell_order_id,ubc.private_key,ubc.public_key,abc.order_id from all_sell_orders aso,all_blockchain_orders abc, user_blockchain_keys ubc where aso.transfer_start_ts ='"+date+" "+time+"' and abc.general_order_id=aso.sell_order_id and abc.order_type='SELL_ORDER' and ubc.user_id  = aso.seller_id and aso.order_status_id=1");
-		 // String query="select aso.sell_order_id,ubc.private_key,ubc.public_key,abc.order_id,abc.all_blockchain_orders_id from all_sell_orders aso,all_blockchain_orders abc, user_blockchain_keys ubc where aso.transfer_start_ts ='"+date+" "+time+"' and abc.general_order_id=aso.sell_order_id and abc.order_type='SELL_ORDER' and ubc.user_id  = aso.seller_id and aso.order_status_id=3";
-		 String query="select a.event_id from all_events a, event_customer_mapping b where a.event_id= \n" + 
-				 "b.event_id and b.event_customer_status_id = 3 and a.event_status_id= 2 and a.event_start_time ='"+date+" "+time+"'";
+		 String query="select a.event_id from all_events a where a.event_status_id= 2 and a.event_start_time ='"+date+" "+time+"'";
+		 // String query="select a.event_id from all_events a where a.event_status_id= 2 and a.event_start_time ='2020-06-21 15:30:00'";
+		 
+//		 String query="select a.event_id from all_events a, event_customer_mapping b where a.event_id= \n" + 
+//				 "b.event_id and (b.event_customer_status_id = 3 || b.event_customer_status_id = 4) and a.event_status_id= 2 and a.event_start_time ='"+date+" "+time+"'";
 //			String query="select a.event_id from all_events a, event_customer_mapping b where a.event_id= \n" + 
-//					"b.event_id and b.event_customer_status_id = 3 and a.event_status_id= 2 and a.event_start_time ='2020-06-08 21:00:00'";
+//					"b.event_id and (b.event_customer_status_id = 3 || b.event_customer_status_id = 4)and a.event_status_id= 2 and a.event_start_time ='2020-06-19 21:00:00'";
+		 	System.out.println(query);
 		 pstmt=con.prepareStatement(query);
 		// pstmt.setString(1,controllerId);
 		 ResultSet rs= pstmt.executeQuery();
-		 ArrayList<HashMap<String,Object>> al=new ArrayList<>();
+		
 		 while(rs.next())
 		 {
 			 HashMap<String,Object> data=new HashMap<>();
@@ -43,7 +48,11 @@ public class ScheduleDAO {
 			// initiateActions(rs.getString("user_id"),rs.getString("status"),rs.getString("controller_id"),rs.getInt("device_id"),"Timer");
 			//topic=rs.getString(1);
 		 }
-		 updateEventsManually(date, time);
+		 // updateEventsManually(date, time);
+		 updateEventsToExpire(date, time);
+	} catch(Exception e) {
+		e.printStackTrace();
+	}
 		return  al;
 	 }
 	 
@@ -64,6 +73,11 @@ public class ScheduleDAO {
 		pstmt.setInt(1,eventId);
 		 pstmt.executeUpdate();
 		 
+		 query="update event_customer_mapping set event_customer_status_id=14 where event_id =? and event_customer_status_id=2";
+		 pstmt=con.prepareStatement(query);
+		pstmt.setInt(1,eventId);
+		 pstmt.executeUpdate();
+		 
 	 }
 	 
 	 public void updateEventsManually(String date, String time) throws SQLException, ClassNotFoundException
@@ -73,8 +87,10 @@ public class ScheduleDAO {
 		 if (con == null ) {
 				con = JDBCConnection.getOracleConnection();
 		 }
+//		 String query="select all_events.event_id,event_customer_mapping.customer_id from all_events,event_customer_mapping where all_events.event_id not in (select a.event_id from all_events a, event_customer_mapping b where a.event_id= \n" + 
+//					"b.event_id and (b.event_customer_status_id =3 || b.event_customer_status_id =4) and a.event_status_id= 2  and a.event_start_time ='"+date+" "+time+"') and all_events.event_status_id= 2  and all_events.event_start_time ='2020-06-19 21:00:00' and all_events.event_id=event_customer_mapping.event_id";
 		 String query="select all_events.event_id,event_customer_mapping.customer_id from all_events,event_customer_mapping where all_events.event_id not in (select a.event_id from all_events a, event_customer_mapping b where a.event_id= \n" + 
-					"b.event_id and (b.event_customer_status_id =3 || b.event_customer_status_id =4) and a.event_status_id= 2  and a.event_start_time ='"+date+" "+time+"') and all_events.event_status_id= 2  and all_events.event_start_time ='"+date+" "+time+"' and all_events.event_id=event_customer_mapping.event_id";
+					"b.event_id and (b.event_customer_status_id =3 || b.event_customer_status_id =4) and a.event_status_id= 2  and a.event_start_time ='"+date+" "+time+"') and all_events.event_status_id= 2  and all_events.event_start_time ='"+date+" "+time+"'' and all_events.event_id=event_customer_mapping.event_id";
 		 
 //		 String query="select a.event_id from all_events a, event_customer_mapping b where a.event_id= \n" + 
 //					"b.event_id and b.event_customer_status_id <> 3 and a.event_status_id= 2 and  a.event_start_time ='2020-06-08 21:00:00'";
@@ -105,5 +121,19 @@ public class ScheduleDAO {
 			
 		}
 		 
+	 }
+	 
+	 
+	 public void updateEventsToExpire(String date, String time) throws SQLException, ClassNotFoundException
+	 {
+		 PreparedStatement pstmt = null;
+		//JDBCConnection connref =new JDBCConnection();
+		 if (con == null ) {
+				con = JDBCConnection.getOracleConnection();
+		 }
+		String query="update all_events set event_status_id=6 where event_status_id =1 and event_start_time ='"+date+" "+time+"'";
+			pstmt=con.prepareStatement(query);
+			 pstmt.executeUpdate();
+			  
 	 }
 }
